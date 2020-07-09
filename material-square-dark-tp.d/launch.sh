b7e2d02ebc57c39c9907d951f0945d0c44c936aa
Joshua Kent <joshua@ideapad-320.home>
2020-07-09 22:21:33 +0100

    transparent modes

---
+++ material-square-dark-tp.d/launch.sh
@@ -0,0 +1,3 @@
+killall -q polybar
+sleep 2
+polybar -c $HOME/.config/polybar/material-square.d/material-square-dark-tp.d/material-square-dark-tp.conf material-square-dark-tp &
---
+++ material-square-light-tp.d/launch.sh
@@ -0,0 +1,3 @@
+killall -q polybar
+sleep 2
+polybar -c $HOME/.config/polybar/material-square.d/material-square-light-tp.d/material-square-light-tp.conf material-square-light-tp &
