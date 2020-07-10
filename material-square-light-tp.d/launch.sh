12745426bb404a259fe9eb20c88ef8585aef0b78
Joshua Kent <joshua@ideapad-320.home>
2020-07-10 16:50:38 +0100

    changed colours in light theme and of i3 workspaces

--- material-square-light-tp.d/launch.sh
+++ material-square-light-tp.d/launch.sh
@@ -1,3 +1,4 @@
 killall -q polybar
 sleep 2
 polybar -c $HOME/.config/polybar/material-square.d/material-square-light-tp.d/material-square-light-tp.conf material-square-light-tp &
+polybar -c $HOME/.config/polybar/material-square.d/material-square-light-tp.d/material-square-light-tp.conf material-square-light-tp-quicklaunch &
