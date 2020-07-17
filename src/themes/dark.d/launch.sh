2fa97c46eb0628d43978be242f4d875f6a9bf3b5
Joshua Kent <hyperxbeat@gmail.com>
2020-07-17 17:38:11 +0100

    moved launch.sh files into one unified file

--- src/themes/dark.d/launch.sh
+++
@@ -1,4 +0,0 @@
-killall -q polybar
-sleep 2
-polybar -c $HOME/.config/polybar/material-square.d/src/themes/dark.d/config.conf material-square-dark &
-polybar -c $HOME/.config/polybar/material-square.d/src/themes/dark.d/config.conf material-square-dark-quicklaunch &
