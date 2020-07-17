48cf5cf888fa71f1bc166d9e6bd43928b7020884
Joshua Kent <hyperxbeat@gmail.com>
2020-07-17 17:38:51 +0100

    fixed commit

--- launch.sh
+++ launch.sh
@@ -1,4 +1,27 @@
-killall -q polybar
-sleep 2
-polybar -c $HOME/.config/polybar/material-square.d/src/themes/material-square-dark-tp.d/material-square-dark-tp.conf material-square-dark-tp &
-polybar -c $HOME/.config/polybar/material-square.d/src/themes/material-square-dark-tp.d/material-square-dark-tp.conf material-square-dark-tp-quicklaunch &
+# OPTIONS
+# dark
+# dark-tp
+# light
+# light-tp
+
+killall -wq polybar
+
+if [[ "$1" == "dark" ]]
+then
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/dark.d/config.conf material-square-dark &
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/dark.d/config.conf material-square-dark-quicklaunch &
+elif [[ "$1" == "dark-tp" ]]
+then
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/dark-tp.d/config.conf material-square-dark-tp &
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/dark-tp.d/config.conf material-square-dark-tp-quicklaunch &
+elif [[ "$1" == "light" ]]
+then
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/light.d/config.conf material-square-light &
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/light.d/config.conf material-square-light-quicklaunch &
+elif [[ "$1" == "light-tp" ]]
+then
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/light-tp.d/config.conf material-square-light-tp &
+	polybar -c $HOME/.config/polybar/material-square.d/src/themes/light-tp.d/config.conf material-square-light-tp-quicklaunch &
+else
+	echo "Invalid input (inputs: 'dark', 'dark-tp', 'light', 'light-tp')"
+fi
