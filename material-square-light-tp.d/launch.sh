e1a3c1cdf6af0b3e9ad32d9a202a3576edc41d2a
Joshua Kent <hyperxbeat@gmail.com>
2020-07-19 18:33:45 +0100

    cleared media

--- material-square-light-tp.d/launch.sh
+++
@@ -1,4 +0,0 @@
-killall -q polybar
-sleep 2
-polybar -c $HOME/.config/polybar/material-square.d/material-square-light-tp.d/material-square-light-tp.conf material-square-light-tp &
-polybar -c $HOME/.config/polybar/material-square.d/material-square-light-tp.d/material-square-light-tp.conf material-square-light-tp-quicklaunch &
--- material-square-light.d/launch.sh
+++
@@ -1,4 +0,0 @@
-killall -q polybar
-sleep 2
-polybar -c $HOME/.config/polybar/material-square.d/material-square-light.d/material-square-light.conf material-square-light &
-polybar -c $HOME/.config/polybar/material-square.d/material-square-light.d/material-square-light.conf material-square-light-quicklaunch &
